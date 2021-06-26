42dc25d98a1a0c40a5a51eeb87255105c1a5d0b3
rootGuillermoF <gerardobec35@gmail.com>
2021-06-26 17:20:28 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cp ./themes/passion.zsh-theme ~/.oh-my-zsh/custom/themes/
+# cp ./themes/passion.zsh-theme ~/.oh-my-zsh/custom/themes/
+ln -s $(pwd)/themes/passion.zsh-theme ~/.oh-my-zsh/custom/themes/
 rm ~/.zshrc
-ln -s pwd/.zshrc ~/.zshrc 
+ln -s $(pwd)/.zshrc ~/.zshrc 
