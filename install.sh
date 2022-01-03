bd295bd7ca757bac316271896382e8e3ca18b516
GuillermoBenqL <gerardobec35@gmail.com>
2022-01-03 17:04:10 -0600

    Add colors for promt and ln -s install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # cp ./themes/passion.zsh-theme ~/.oh-my-zsh/custom/themes/
-ln -s $(pwd)/themes/passion.zsh-theme ~/.oh-my-zsh/custom/themes/passion.zsh-theme
 rm ~/.zshrc
 ln -s $(pwd)/.zshrc ~/.zshrc 
+ln -s $(pwd)/themes/passion.zsh-theme ~/.oh-my-zsh/custom/themes/passion.zsh-theme
+ln -s $(pwd)/themes/passion2.zsh-theme ~/.oh-my-zsh/custom/themes/passion2.zsh-theme
+ln -s $(pwd)/themes/dev.zsh-theme ~/.oh-my-zsh/custom/themes/dev.zsh-theme
